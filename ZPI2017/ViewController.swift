d5affd278d844472fb55db3352f2a7f039620f6e
Krzysztof Grys <krzysztofgrys@krzysztofs-macbook-pro.local>
2017-03-13 15:38:20 +0100

    Initial Commit

---
+++ ZPI2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZPI2017
+//
+//  Created by Krzysztof Grys on 3/13/17.
+//  Copyright © 2017 ZPI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
