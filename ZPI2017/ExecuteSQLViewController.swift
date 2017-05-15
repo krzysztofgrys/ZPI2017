ac37c9308759d832cf1832df05b52fcf85edfe61
piotrm3 <piotr_m3@op.pl>
2017-05-15 15:42:43 +0200

    Execute SQL

---
+++ ZPI2017/ExecuteSQLViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ExecuteSQLViewController.swift
+//  ZPI2017
+//
+//  Created by Administrator on 15.05.2017.
+//  Copyright © 2017 ZPI. All rights reserved.
+//
+
+import UIKit
+
+class ExecuteSQLViewController: UIViewController {
+
+    @IBOutlet weak var SQLTextField: UITextView!
+    @IBAction func executeButtonClicked(_ sender: AnyObject) {
+        NSLog("execute Button Clicked")
+        NSLog(SQLTextField.text)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
